aa20b7e21b7cf3271f2e78a8ea81d4ce7bc1b2c9
Rashika Patel <rashika_patel@hotmail.com>
2018-10-19 13:33:44 +0100

    add index Model

--- app/indexModel.js
+++ app/indexModel.js
@@ -2,6 +2,8 @@
   function IndexModel() {
   }
   IndexModel.prototype.queryLocalStorage = function() {
+
+    return [{'url': "www.bbc.co.uk", 'duration': 10000}]
   }
 
   exports.IndexModel = IndexModel
