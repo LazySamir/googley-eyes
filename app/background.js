4fd225ba4a6e2ed011154262b0a468d2200dfe09
Samir Soormally <samirsoormally@gmail.com>
2018-10-19 12:13:50 +0100

    refactor: updated crx file and references in tests

--- app/background.js
+++ app/background.js
@@ -1,6 +1,3 @@
-function getUrl() {
-
-}
-
-// define our event listeners
-chrome.tabs.onActivated.addListener(getUrl);
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
+});
