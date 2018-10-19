f2f7ba106dd478caf8653425cdc6de9b547363a4
Samir Soormally <samirsoormally@gmail.com>
2018-10-19 14:07:49 +0100

    fix: background.js format

--- app/background.js
+++ app/background.js
@@ -1,3 +1,6 @@
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
-});
+function getUrl() {
+
+}
+
+// define our event listeners
+chrome.tabs.onActivated.addListener(getUrl);
