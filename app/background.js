d2a323a65626a49c057c6c1634695d0e673a58d5
davidlawes <davidlawes@hotmail.co.uk>
2018-10-20 08:43:32 +0100

    test: added unit test for HandleUpdate()

--- app/background.js
+++ app/background.js
@@ -1,8 +1,19 @@
-function getUrl() {
-}
-// define our event listeners
-chrome.tabs.onActivated.addListener(getUrl);
+function HandleUpdate(tabId, changeInfo) {
+  console.log(changeInfo.url);
+  chrome.storage.sync.get({allData: []}, function(result) {
+    var allDataArray = result.allData;
+    allDataArray.push({ url: changeInfo.url });
+    chrome.storage.sync.set({allData: allDataArray}, function() {
+      chrome.storage.sync.get("allData", function(result) {})
+      console.log(result.allData);
+    })
+  })
+};
 
 chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
+  chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  HandleUpdate(tabId, changeInfo);
 });
--- spec/unit/backgroundSpec.js
+++ spec/unit/backgroundSpec.js
@@ -9,14 +9,40 @@ const code = fs.readFileSync('./app/background.js');
 
 describe("Background JS", function() {
 
-  describe("defines our event listeners", function() {
+  describe("defines event listeners", function() {
 
-    it("creates an addListener() for chrome.tabs.onActivated", function() {
-      spyOn(chrome.tabs.onActivated, 'addListener');
+    it("creates an addListener() for chrome.browserAction.onClicked", function() {
+      spyOn(chrome.browserAction.onClicked, "addListener");
       vm.runInNewContext(code, context);
-      expect(chrome.tabs.onActivated.addListener).toHaveBeenCalled();
-    });
+      expect(chrome.browserAction.onClicked.addListener).toHaveBeenCalled();
+    })
+
+    it("creates an addListener() for chrome.tabs.onUpdated", function() {
+      spyOn(chrome.tabs.onUpdated, "addListener");
+      vm.runInNewContext(code, context);
+      expect(chrome.tabs.onUpdated.addListener).toHaveBeenCalled();
+    })
 
   });
+
+  describe("HandleUpdate()", function() {
+
+      it("gets data from chrome.storage", function() {
+        chrome.tabs.onUpdated.dispatch(1234, { url: "test" });
+        vm.runInNewContext(code, context);
+        expect(chrome.storage.sync.get.calledOnce).toEqual(true);
+      });
+
+      it("pushes updated array into chrome.storage", function() {
+        chrome.tabs.onUpdated.dispatch(1234, { url: "test"});
+        vm.runInNewContext(code, context);
+        expect(chrome.storage.sync.set.withArgs({ allData: [ {url: "test"} ]}, function(){}).calledOnce).toEqual(true);
+      });
+
+  });
+
+  afterAll(function() {
+    chrome.flush();
+  })
 
 });
