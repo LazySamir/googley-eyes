645a747eddedb5223961a91db9fc870d3e15dd00
davidlawes <davidlawes@hotmail.co.uk>
2018-10-20 09:28:58 +0100

    test: refactor tests for HandleUpdate()

--- spec/unit/backgroundSpec.js
+++ spec/unit/backgroundSpec.js
@@ -3,7 +3,6 @@
 const vm = require('vm');
 const fs = require('fs');
 const chrome = require("sinon-chrome")
-
 const context = { chrome: chrome };
 const code = fs.readFileSync('./app/background.js');
 
@@ -11,38 +10,45 @@ describe("Background JS", function() {
 
   describe("defines event listeners", function() {
 
-    it("creates an addListener() for chrome.browserAction.onClicked", function() {
-      spyOn(chrome.browserAction.onClicked, "addListener");
+    beforeEach(function() {
       vm.runInNewContext(code, context);
-      expect(chrome.browserAction.onClicked.addListener).toHaveBeenCalled();
-    })
+    });
+
+    it("creates an addListener() for chrome.browserAction.onClicked", function() {
+      expect(chrome.browserAction.onClicked.addListener.called).toEqual(true);
+    });
 
     it("creates an addListener() for chrome.tabs.onUpdated", function() {
-      spyOn(chrome.tabs.onUpdated, "addListener");
-      vm.runInNewContext(code, context);
-      expect(chrome.tabs.onUpdated.addListener).toHaveBeenCalled();
+      expect(chrome.tabs.onUpdated.addListener.called).toEqual(true);
+    });
+
+    afterAll(function() {
+      chrome.flush();
     })
 
   });
 
   describe("HandleUpdate()", function() {
 
-      it("gets data from chrome.storage", function() {
-        chrome.tabs.onUpdated.dispatch(1234, { url: "test" });
-        vm.runInNewContext(code, context);
-        expect(chrome.storage.sync.get.calledOnce).toEqual(true);
-      });
+    beforeEach(function() {
+      vm.runInNewContext(code, context);
+      chrome.tabs.onUpdated.dispatch(1234, { url: "test" });
+      chrome.storage.sync.get.yields({ allData: [] });
+    });
 
-      it("pushes updated array into chrome.storage", function() {
-        chrome.tabs.onUpdated.dispatch(1234, { url: "test"});
-        vm.runInNewContext(code, context);
-        expect(chrome.storage.sync.set.withArgs({ allData: [ {url: "test"} ]}, function(){}).calledOnce).toEqual(true);
-      });
+    it("gets data from chrome.storage", function() {
+      expect(chrome.storage.sync.get.called).toEqual(true);
+    });
+
+    it("pushes updated array into chrome.storage", function() {
+      console.log(chrome.storage.sync.set);
+      expect(chrome.storage.sync.set.called).toEqual(true);
+    });
+
+    afterAll(function() {
+      chrome.flush();
+    })
 
   });
-
-  afterAll(function() {
-    chrome.flush();
-  })
 
 });
