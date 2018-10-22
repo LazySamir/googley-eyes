d48b389b40b3e59b5cf064eb01bb64a71eb88eb5
davidlawes <davidlawes@hotmail.co.uk>
2018-10-22 12:15:32 +0100

    test: removed pauses from FeaturesSpec - all tests still passing

--- spec/features/FeaturesSpec.js
+++ spec/features/FeaturesSpec.js
@@ -7,10 +7,8 @@ let app_url = 'chrome-extension://loindgcbdjommoaimpnmkhnnbhpeoolh/index.html'
   describe("Extension is loaded correctly", function() {
 
     it("index.html can be opened", function() {
-      browser.url(app_url)
-      browser.pause(200);
+      browser.url(app_url);
       expect(browser.getTitle()).toEqual("Googley Eyes");
-      browser.pause(100);
     });
 
   });
@@ -19,9 +17,8 @@ let app_url = 'chrome-extension://loindgcbdjommoaimpnmkhnnbhpeoolh/index.html'
 
     it("displays visited URLs", function() {
       browser.url("https://www.bbc.co.uk");
-      browser.url(app_url)
+      browser.url(app_url);
       expect(browser.getText("#url_container")).toContain("https://www.bbc.co.uk");
-      browser.pause(100)
     });
 
   });
@@ -29,12 +26,9 @@ let app_url = 'chrome-extension://loindgcbdjommoaimpnmkhnnbhpeoolh/index.html'
   describe("New Tab View", function() {
 
     it("displays latest URLs", function() {
-      // browser.pause(100);
       browser.url("https://www.bbc.co.uk");
-      browser.url("chrome://newtab/")
-      browser.pause(200)
+      browser.url("chrome://newtab/");
       expect(browser.getText("#links-container")).toContain("https://www.bbc.co.uk/");
-      // browser.pause(10000)
     });
 
   });
