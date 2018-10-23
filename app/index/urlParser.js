b5a16e53804a805dc2d996ecd14997a72db3e16e
davidlawes <davidlawes@hotmail.co.uk>
2018-10-23 11:28:15 +0100

    feature: function parseUrl added to UrlParser

--- app/index/urlParser.js
+++ app/index/urlParser.js
@@ -1,9 +1,14 @@
+"use strict";
+const URL = require('url-parse');
+
 (function(exports) {
 
-  UrlParser = function(){};
+  function UrlParser(){};
 
-
+  UrlParser.prototype.parseUrl = function(url) {
+    let parsedUrl = new URL(url);
+    return parsedUrl.hostname;
+  };
 
   exports.UrlParser = UrlParser;
-
 })(this);
--- spec/unit/urlParserSpec.js
+++ spec/unit/urlParserSpec.js
@@ -1,15 +1,17 @@
+"use strict";
+
 const U = require("../../app/index/urlParser");
+const urlParser = new U.UrlParser();
+const URL = require('url-parse');
 
 describe("UrlParser", function() {
-
-  let urlParser = new U.UrlParser();
 
   describe(".parseUrl()", function() {
 
     it("returns the domain name of the url", function() {
-      expect(urlParser.parseUrl("https://github.com/LazySamir/googley-eyes/pull/53")).toEqual("https://github.com")
+      expect(urlParser.parseUrl("https://github.com/LazySamir/googley-eyes/pull/53")).toEqual("github.com")
     });
 
-  })
+  });
 
-})
+});
