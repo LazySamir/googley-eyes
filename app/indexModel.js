6e739212d4f0237b15fe104630aaa1eeb8a3a7d7
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-10-23 13:58:32 +0100

    feat: add duration to display

--- app/indexModel.js
+++ app/indexModel.js
@@ -9,5 +9,27 @@
     })
   }
 
+  IndexModel.prototype.convertDuration = function(milliseconds) {
+    var seconds = milliseconds /1000;
+    var hours = parseInt( seconds / 3600);
+    seconds = seconds % 3600;
+
+    var minutes = parseInt ( seconds / 60 );
+    seconds = parseInt(seconds % 60);
+
+    hours = this.addZero(hours);
+    minutes = this.addZero(minutes);
+    seconds = this.addZero(seconds);
+
+    return `${hours}:${minutes}:${seconds}`
+  }
+
+  IndexModel.prototype.addZero = function(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+  }
+
   exports.IndexModel = IndexModel
 })(this)
