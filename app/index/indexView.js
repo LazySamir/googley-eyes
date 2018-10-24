fad4036133fda8d77d21f7757b008a45b6f2f0c2
davidlawes <davidlawes@hotmail.co.uk>
2018-10-24 10:57:03 +0100

    style: updated containers on index.html

--- app/index/indexView.js
+++ app/index/indexView.js
@@ -5,11 +5,11 @@
   IndexView.prototype.getHTML = function(data) {
     console.log("getHTML");
     console.log(data);
-    let output = "<ul>"
+    let output = '<table id="domain-time-table"><tr><th>Domain</th><th>Time</th></tr>'
     for(let i = 0; i < data.length; i++) {
-     output += `<li><div>${data[i]['url']}: ${this.model.convertDuration(data[i]['duration'])}</div></li>`
+     output += `<tr><td>${data[i]['url']}</td><td>${this.model.convertDuration(data[i]['duration'])}</td></tr>`
     }
-    output += "</ul>"
+    output += "</table>"
     return output
   }
 
