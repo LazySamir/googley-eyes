1734ec923224dfdaf043c67738a77ae3f4b9a47e
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-10-24 10:59:32 +0100

    feat: convert data to pie chart data format

--- app/index/chartDataConverter.js
+++ app/index/chartDataConverter.js
@@ -13,5 +13,22 @@
     return columnData
   };
 
+  ChartDataConverter.prototype.convertToPie = function(data) {
+    let pieData = []
+    let totalDuration = 0
+    data.forEach(function(el) {
+      totalDuration += el.duration
+    })
+
+    data.forEach(function(el) {
+      pieData.push({ label: el.url, y: toPercentage(totalDuration, el.duration) })
+    })
+    return pieData
+  }
+
+  function toPercentage(totalDuration, duration) {
+    return  Math.round( ((duration / totalDuration) * 100) * 1e2 ) / 1e2;
+  }
+
   exports.ChartDataConverter = ChartDataConverter;
 })(this);
