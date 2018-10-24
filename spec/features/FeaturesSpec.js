44937bd1a9bce51b5b47c86b5f61eacd0cd5161e
Samir Soormally <samirsoormally@gmail.com>
2018-10-24 17:14:55 +0100

    chore: updated manifest to 1.8

--- spec/features/FeaturesSpec.js
+++ spec/features/FeaturesSpec.js
@@ -2,7 +2,7 @@
 
 describe("Features Spec", function() {
 
-let app_url = 'chrome-extension://daldbbjlfghpofhnokohphnibodahckg/index.html'
+let app_url = 'chrome-extension://lhibnhgnknlgkeebajipkijniihoholg/index.html'
 
   describe("Extension is loaded correctly", function() {
 
